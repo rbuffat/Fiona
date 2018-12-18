dd77b7e057789fdfa6df933690d432ed92891892
René Buffat <buffat@gmail.com>
2018-12-18 13:19:07 +0100

    support gdal rc versions in travis

--- scripts/travis_gdal_install.sh
+++ scripts/travis_gdal_install.sh
@@ -70,7 +70,7 @@ elif [ ! -d "$GDALINST/gdal-$GDALVERSION" ]; then
   # only build if not already installed
   cd $GDALBUILD
 
-  BASE_GDALVERSION = $(sed 's/[a-zA-Z].*//g' <<< $GDALVERSION)
+  BASE_GDALVERSION=$(sed 's/[a-zA-Z].*//g' <<< $GDALVERSION)
 
   if ( curl -o/dev/null -sfI "http://download.osgeo.org/gdal/$BASE_GDALVERSION/gdal-$GDALVERSION.tar.gz" ); then
     wget http://download.osgeo.org/gdal/$BASE_GDALVERSION/gdal-$GDALVERSION.tar.gz
