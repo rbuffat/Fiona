8f780b3188980fc63cc4be81d358f03f3cbcfaf0
René Buffat <buffat@gmail.com>
2020-02-27 17:52:00 +0100

    add test

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -899,3 +899,39 @@ def test_collection_env(path_coutwildrnp_shp):
     """We have a GDAL env within collection context"""
     with fiona.open(path_coutwildrnp_shp):
         assert 'FIONA_ENV' in getenv()
+
+
+write_not_append_drivers = [driver for driver, raw in supported_drivers.items() if 'w' in raw and not 'a' in raw]
+
+@pytest.mark.parametrize('driver', write_not_append_drivers)
+def test_append_does_not_work(tmpdir, driver):
+    """Test if driver supports append but it is not enabled
+    
+    If this test fails, it should be considered to enable append for the respective driver in drvsupport.py. 
+    
+    """
+
+    backup_mode = supported_drivers[driver]
+
+    supported_drivers[driver] = 'raw'
+
+    extension = driver_extensions.get(driver, "bar")
+    path = str(tmpdir.join('foo.{}'.format(extension)))
+
+    with fiona.open(path, 'w',
+                    driver=driver,
+                    schema={'geometry': 'LineString',
+                            'properties': [('title', 'str')]}) as c:
+
+        c.writerecords([{'geometry': {'type': 'LineString', 'coordinates': [
+                       (1.0, 0.0), (0.0, 0.0)]}, 'properties': {'title': 'One'}}])
+
+    with pytest.raises(Exception):
+        with fiona.open(path, 'a',
+                    driver=driver) as c:
+            c.writerecords([{'geometry': {'type': 'LineString', 'coordinates': [
+                        (2.0, 0.0), (0.0, 0.0)]}, 'properties': {'title': 'Two'}}])
+
+    supported_drivers[driver] = backup_mode
+
+
