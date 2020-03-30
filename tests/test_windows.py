3291775a08ba098c715d06bb3464b2913ab1a419
René Buffat <buffat@gmail.com>
2020-03-30 21:05:39 +0200

    fix monkeypatch

--- tests/test_windows.py
+++ tests/test_windows.py
@@ -20,7 +20,7 @@ def test_import_gdalhome(monkeypatch):
         if 'gdal' not in path.lower():
             paths.append(path)
 
-    monkeypatch.setenv("PATH", os.pathsep.join(paths), raising=True)
+    monkeypatch.setenv("PATH", os.pathsep.join(paths))
 
     import  fiona
 
