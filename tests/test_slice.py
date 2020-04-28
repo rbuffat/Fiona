2b9fa12fc050f725f5c8d5e38190461722ff6385
René Buffat <buffat@gmail.com>
2020-04-28 22:26:34 +0200

    update slice test

--- tests/test_slice.py
+++ tests/test_slice.py
@@ -5,9 +5,12 @@ import logging
 import sys
 
 import pytest
-
+from fiona.env import GDALVersion
 import fiona
 from fiona.errors import FionaDeprecationWarning
+from .conftest import get_temp_filename
+from fiona.drvsupport import supported_drivers, driver_mode_mingdal
+
 
 def test_collection_get(path_coutwildrnp_shp):
     with fiona.open(path_coutwildrnp_shp) as src:
@@ -39,43 +42,57 @@ def test_collection_iterator_next(path_coutwildrnp_shp):
         assert v['id'] == '5'
 
 
-def test_collection_iterator_items_slice(path_coutwildrnp_shp):
+@pytest.mark.parametrize('driver', [driver for driver, raw in supported_drivers.items() if 'w' in raw
+                                    and driver not in {'DGN', 'MapInfo File', 'GPSTrackMaker', 'GPX', 'BNA', 'DXF',
+                                                       'GML'}])
+@pytest.mark.parametrize("args", [((0, 5, None), 5),
+                                  ((1, 5, None), 4),
+                                  ((-5, None, None), 5),
+                                  ((-5, -1, None), 4),
+                                  ((0, None, None), 10),
+                                  ((5, None, None), 10-5),
+                                  ((0, 5, 2), 3),
+                                  ((1, 5, 2), 2),
+                                  ((-5, None, 2), 3),
+                                  ((-5, -1, 2), 2),
+                                  ((0, None, 2), 5),
+                                  ((5, None, 2), 3),
+                                  ((5, None, -1), 6),
+                                  ((5, None, -2), 3),
+                                  ((5, None, None), 5),
+                                  ((4, None, -2), 3),
+                                  ((-1, -5, -1), 4),
+                                  ((-5, None, -1), 10 - 5 + 1)])
+def test_collection_iterator_items_slice(tmpdir, driver, args):
+    """ Test if c.filter(start, stop) returns the correct features.
+    """
 
-    with fiona.open(path_coutwildrnp_shp) as src:
-        count = len(src)
+    arg1, count = args
+    start, stop, step = arg1
 
-        items = list(src.items(0, 5))
-        assert len(items) == 5
+    min_id = 0
+    max_id = 9
 
-        items = list(src.items(1, 5))
-        assert len(items) == 4
+    schema = {'geometry': 'Point', 'properties': [('position', 'int')]}
+    path = str(tmpdir.join(get_temp_filename(driver)))
 
-        items = list(src.items(-5, None))
-        assert len(items) == 5
+    # We only test driver with write capabilities
+    if driver in driver_mode_mingdal['w'] and GDALVersion.runtime() < GDALVersion(
+            *driver_mode_mingdal['w'][driver][:2]):
+        return
 
-        items = list(src.items(-5, -1))
-        assert len(items) == 4
+    records = [{'geometry': {'type': 'Point', 'coordinates': (0.0, float(i))}, 'properties': {'position': i}} for i in
+               range(min_id, max_id + 1)]
 
-        items = list(src.items(0, None))
+    # Create test file to append to
+    with fiona.open(path, 'w',
+                    driver=driver,
+                    schema=schema) as c:
+        c.writerecords(records)
+
+    with fiona.open(path, 'r') as c:
+        items = list(c.items(start, stop, step))
         assert len(items) == count
-
-        items = list(src.items(5, None))
-        assert len(items) == (count - 5)
-
-        items = list(src.items(5, None, -1))
-        assert len(items) == 6
-
-        items = list(src.items(5, None, -2))
-        assert len(items) == 3
-
-        items = list(src.items(4, None, -2))
-        assert len(items) == 3
-
-        items = list(src.items(-1, -5, -1))
-        assert len(items) == 4
-
-        items = list(src.items(-5, None, -1))
-        assert len(items) == (count - 5 + 1)
 
 
 def test_collection_iterator_keys_next(path_coutwildrnp_shp):
