1536dec4ce564b69bc0f556d9fb5cb6b0bc92aa5
René Buffat <buffat@gmail.com>
2020-05-13 17:20:04 +0200

    fix test_append_memoryfile

--- tests/test_memoryfile.py
+++ tests/test_memoryfile.py
@@ -49,7 +49,7 @@ def test_write_memoryfile(profile_first_coutwildrnp_shp):
 
 
 @pytest.mark.parametrize('driver', [driver for driver in ['GeoJSON', 'GPKG', 'ESRI Shapefile'] if (
-        'a' in supported_drivers['driver'] and driver not in driver_mode_mingdal['a'] or
+        'a' in supported_drivers[driver] and driver not in driver_mode_mingdal['a'] or
         gdal_version >= GDALVersion(*driver_mode_mingdal['w'][driver][:2]))])
 def test_append_memoryfile(driver):
     """In-memory Shapefile can be appended"""
