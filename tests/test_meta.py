3dd85b89ac4cac29f61c87934bd63759091aefa3
René Buffat <buffat@gmail.com>
2020-06-05 18:12:44 +0200

    update test

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -25,3 +25,10 @@ def test_extensions(driver):
 def test_supports_vsi(driver):
     # do not fail
     assert fiona.meta.supports_vsi(driver) in (True, False)
+
+
+@requires_gdal2
+@pytest.mark.parametrize("driver", supported_drivers)
+def test_supported_field_types(driver):
+    # do not fail
+    isinstance(fiona.meta.extensions(driver), list)
