e668c509fe72b3cce9d0290040c386f22c8020a0
René Buffat <buffat@gmail.com>
2020-06-05 16:13:19 +0200

    remove2

--- fiona/_meta.pyx
+++ fiona/_meta.pyx
@@ -1,4 +1,3 @@
-# cython: c_string_type=unicode, c_string_encoding=utf8
 from __future__ import absolute_import
 
 include "gdal.pxi"
@@ -8,42 +7,8 @@ from fiona.compat import strencode
 from fiona._shim cimport gdal_open_vector
 from fiona.env import ensure_env
 import logging
-
-from fiona._geometry cimport (
-    GeomBuilder, OGRGeomBuilder, geometry_type_code,
-    normalize_geometry_type_code, base_geometry_type_code)
-from fiona._err cimport exc_wrap_int, exc_wrap_pointer, exc_wrap_vsilfile, get_last_error_msg
-
-import fiona
-from fiona._env import GDALVersion, get_gdal_version_num
-from fiona._err import cpl_errs, FionaNullPointerError, CPLE_BaseError, CPLE_OpenFailedError
-from fiona._geometry import GEOMETRY_TYPES
-from fiona import compat
-from fiona.errors import (
-    DriverError, DriverIOError, SchemaError, CRSError, FionaValueError,
-    TransactionError, GeometryTypeValidationError, DatasetDeleteError,
-    FionaDeprecationWarning)
-from fiona.compat import OrderedDict, strencode
-from fiona.rfc3339 import parse_date, parse_datetime, parse_time
-from fiona.rfc3339 import FionaDateType, FionaDateTimeType, FionaTimeType
-from fiona.schema import FIELD_TYPES, FIELD_TYPES_MAP, normalize_field_type
-from fiona.path import vsi_path
-from fiona._shim cimport is_field_null, osr_get_name, osr_set_traditional_axis_mapping_strategy
-
-from libc.stdlib cimport malloc, free
-from libc.string cimport strcmp
-from cpython cimport PyBytes_FromStringAndSize, PyBytes_AsString
-
-
-cdef extern from "ogr_api.h" nogil:
-
-    ctypedef void * OGRLayerH
-    ctypedef void * OGRDataSourceH
-    ctypedef void * OGRSFDriverH
-    ctypedef void * OGRFieldDefnH
-    ctypedef void * OGRFeatureDefnH
-    ctypedef void * OGRFeatureH
-    ctypedef void * OGRGeometryH
+from fiona._shim cimport *
+from six import integer_types, string_types, text_type
 
 
 log = logging.getLogger(__name__)
