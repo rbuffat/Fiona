8d98965bf615158279ef2834d29558285515fc45
René Buffat <buffat@gmail.com>
2020-08-18 16:26:47 +0200

    add extension

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,7 +27,8 @@ driver_extensions = {'DXF': 'dxf',
                      'GeoJSONSeq': 'geojsons',
                      'GMT': 'gmt',
                      'OGR_GMT': 'gmt',
-                     'BNA': 'bna'}
+                     'BNA': 'bna',
+                     'FlatGeobuf': 'fgb'}
 
 
 def pytest_report_header(config):
