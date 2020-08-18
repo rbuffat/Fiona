11b5e8b7f978c45282e3b6a688b5fe46e7247be2
René Buffat <buffat@gmail.com>
2020-08-18 12:38:16 +0200

    fix library path

--- scripts/travis_filegdb_install.sh
+++ scripts/travis_filegdb_install.sh
@@ -6,13 +6,12 @@ if [ ! -d "$FILEGDB" ]; then
   cd $FILEGDB
   wget -q https://github.com/Esri/file-geodatabase-api/raw/master/FileGDB_API_1.5.1/FileGDB_API_1_5_1-64gcc51.tar.gz
   tar -xzf FileGDB_API_1_5_1-64gcc51.tar.gz --strip=1 FileGDB_API-64gcc51
-  ls -l
   rm FileGDB_API_1_5_1-64gcc51.tar.gz
   rm -rf samples
   rm -rf doc
-  ls -l
-  export LD_LIBRARY_PATH=$FILEGDB/lib:$LD_LIBRARY_PATH
 fi
+
+export LD_LIBRARY_PATH=$FILEGDB/lib:$LD_LIBRARY_PATH
 
 # change back to travis build dir
 cd $TRAVIS_BUILD_DIR
